012a4cb533e35f1bc0b1c2ae4cf86ba73b708586
Dhevan Lau <dhevan999@gmail.com>
2021-11-17 17:31:10 +0200

    Added TestDao class

--- app/src/main/java/com/example/ashleyturnbull_comp304sec003_lab4/TestDao.java
+++ app/src/main/java/com/example/ashleyturnbull_comp304sec003_lab4/TestDao.java
@@ -1,5 +1,30 @@
 package com.example.ashleyturnbull_comp304sec003_lab4;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import java.util.List;
+
+@Dao
 public interface TestDao {
-    
+    @Insert
+    void insert(Test test);
+
+    @Update
+    void update(Test test);
+
+    @Delete
+    void delete(Test test);
+
+    @Query("DELETE FROM test_table")
+    void deleteAll();
+
+    @Query("Select * FROM test_table where testID = :testID")
+    LiveData<Test> getByTestID(int testID);
+
+    @Query("Select * FROM test_table")
+    LiveData<List<Test>> getAllTests();
 }
