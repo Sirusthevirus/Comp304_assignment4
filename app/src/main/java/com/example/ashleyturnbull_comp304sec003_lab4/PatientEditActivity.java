e00230f9ea862406c2774cb681d2aeaf666a8f9e
Dhevan Lau <dhevan999@gmail.com>
2021-11-17 23:05:14 +0200

    Revert "Merge branch 'main' of https://github.com/Sirusthevirus/Comp304_assignment4"
    This reverts commit 0f52394a727c32425c18bd8624eae10b0ffd90f2, reversing
    changes made to 133daaa580fe725b308b0f2e9b3a731a0d9ade8f.

---
+++ app/src/main/java/com/example/ashleyturnbull_comp304sec003_lab4/PatientEditActivity.java
@@ -0,0 +1,14 @@
+package com.example.ashleyturnbull_comp304sec003_lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_edit);
+    }
+}
