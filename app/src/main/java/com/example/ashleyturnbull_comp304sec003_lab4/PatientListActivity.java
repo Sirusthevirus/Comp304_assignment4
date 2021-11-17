c74d8192beb20737345fdf68a72bfc7aac3a928a
Ashley T <28864931+Sirusthevirus@users.noreply.github.com>
2021-11-17 15:23:45 -0500

    added logic for edit Patient

--- app/src/main/java/com/example/ashleyturnbull_comp304sec003_lab4/PatientListActivity.java
+++ app/src/main/java/com/example/ashleyturnbull_comp304sec003_lab4/PatientListActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -19,11 +20,17 @@ import java.util.List;
 public class PatientListActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
         private PatientViewModel patientViewModel;
+        private EditText firstName, lastName, department, nurseID, room;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_patient_list);
+                firstName = (EditText)findViewById(R.id.editText_FirstName);
+                lastName = (EditText)findViewById(R.id.editText_LastName);
+                department = (EditText)findViewById(R.id.editText_Department);
+                nurseID = (EditText)findViewById(R.id.editText_NurseID);
+                room = (EditText)findViewById(R.id.editText_Room);
 
                 patientViewModel = new ViewModelProvider(this).get(PatientViewModel.class);
                 //Spinner
@@ -49,7 +56,19 @@ public class PatientListActivity extends AppCompatActivity implements AdapterVie
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                patientViewModel.getAllPatients().observe(this, new Observer<List<Patient>>() {
+                        @Override
+                        public void onChanged(@Nullable List<Patient> result) {
 
+                                for (Patient patient : result) {
+                                        firstName.setText(patient.getFirstName());
+                                        lastName.setText(patient.getLastName());
+                                        department.setText(patient.getDepartment());
+                                        nurseID.setText(patient.getNurseID());
+                                        room.setText(patient.getRoom());
+                                }
+                        }
+                });
         }
 
         @Override
